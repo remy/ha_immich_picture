db8639d84c95e7de70a4c7831d46aefc96987582
Remy Sharp <remy@remysharp.com>
2025-01-20 23:18:14 +0000

    better performance

--- index.mjs
+++ index.mjs
@@ -3,11 +3,33 @@ import puppeteer from 'puppeteer';
 import { existsSync } from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import 'renvy';
 
 const app = express();
 const PORT = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const SCRIPTS_DIR = join(__dirname, 'scripts');
+
+let now = Date.now();
+
+const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process',
+    '--disable-gpu',
+  ],
+});
+
+app.use('/reset', (req, res) => {
+  now = Date.now();
+  res.json({ message: 'Cache reset' });
+});
 
 // Middleware to dynamically route based on script files in 'scripts' folder
 app.use('/api/:scriptName', async (req, res) => {
@@ -20,10 +42,10 @@ app.use('/api/:scriptName', async (req, res) => {
   if (existsSync(scriptPath)) {
     try {
       // Dynamically import the script, forcing a fresh reload
-      const script = await import(scriptPath + `?cacheBust=${Date.now()}`);
+      const script = await import(scriptPath + `?cacheBust=${now}`);
 
       // Run the script and pass Puppeteer instance
-      const result = await script.run(req, res, puppeteer);
+      const result = await script.run(req, res, browser);
       console.log(`result: ${JSON.stringify(result)}`);
       res.json(result);
     } catch (error) {
